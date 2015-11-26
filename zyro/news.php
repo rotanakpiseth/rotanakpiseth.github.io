dd1adcbc0e28f20825e7e8f5015bb404790c6258
rotanakpiseth <piseth9@gmail.com>
2015-11-26 22:24:18 +0700

    Hello
    Welcome

---
+++ zyro/news.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>News</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="News" />
+		<meta name="generator" content="Zyro - Website Builder" />
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.0.13" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1442331101" rel="stylesheet" type="text/css" />
+	<link href="css/news.css?ts=1442331101" rel="stylesheet" type="text/css" />
+	
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_main" style="height: 197px;">
+	
+<div id="wb_element_instance10" class="wb_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle">Welcome</h4>
+</div><div id="wb_element_instance11" class="wb_element" style=" line-height: normal;"><p style="text-align: right;">to my source</p>
+</div><div id="wb_element_instance12" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2015 <a href="http://rotanakpiseth.grn.cc">rotanakpiseth.grn.cc</a></p></div><div id="wb_element_instance13" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(news);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance13");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance13").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div><div id="wb_element_instance14" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.height(0);
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div></body>
+</html>
