2c46773adb6da8226cf1ba68b7523c38793228a4
rotanakpiseth <piseth9@gmail.com>
2021-07-02 09:51:25 +0700

    Update Mgidheader.js
    Update

--- Mgidheader.js
+++ Mgidheader.js
@@ -1,14 +1,10 @@
 <!-- Composite Start -->
 <div id="M726886ScriptRootC1160278">
-        <div id="M726886ScriptRootC1160278">
-        Loading...    </div>
+    <div id="M726886ScriptRootC1160278"> Loading...
     </div>
-<script>
-        (function () {
-        var script = document.createElement('script');
-        script.src = "//jsc.mgid.com/b/i/namina_kiky.com.780378.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
-        script.async = true;
-        document.body.appendChild(script);
-    })();
-</script>
+    </div>
+    <script>
+(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/b/l/bloggue.web.id.778521.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
+    </script>
 <!-- Composite End -->
+
